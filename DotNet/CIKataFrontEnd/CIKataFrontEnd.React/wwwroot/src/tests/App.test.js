de8467079a67967ae91df32ee43e3eb38e44cbc0
John MacGregor <jmacgregor@pillartechnology.com>
2019-05-13 11:39:35 -0400

    Tests should pass for the right reason now

--- DotNet/CIKataFrontEnd/CIKataFrontEnd.React/wwwroot/src/tests/App.test.js
+++ DotNet/CIKataFrontEnd/CIKataFrontEnd.React/wwwroot/src/tests/App.test.js
@@ -4,10 +4,10 @@ import App from '../Components/App';
 import "isomorphic-fetch";
 
 describe('App', () => {
-    it('should contain a div', () => {
+    it('should contain a button', () => {
         const component = shallow(<App />);
 
-        expect(component.find('h1').length).toBe(1);
+        expect(component.find('button').length).toBe(1);
     })
 });
 
