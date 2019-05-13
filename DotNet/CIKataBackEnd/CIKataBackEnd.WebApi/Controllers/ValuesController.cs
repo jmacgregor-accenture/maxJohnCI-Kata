4ed5a5c6e75fa4a9ee16c393b25009e39c0514e2
John MacGregor <jmacgregor@pillartechnology.com>
2019-05-13 10:54:59 -0400

    Adding missing pieces of the backend project

---
+++ DotNet/CIKataBackEnd/CIKataBackEnd.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using CIKataBackEnd.WebApi.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RONCBackEnd.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private BusinessLogicService _businessLogicService = new BusinessLogicService();
+
+        // GET api/values/
+        [HttpGet]
+        public JsonResult Get()
+        {
+            HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+
+            var returnString = _businessLogicService.MakeUpValue();
+
+            if (string.IsNullOrEmpty(returnString))
+            {
+                return new JsonResult("Call Did Not Work");
+            }
+            
+            return new JsonResult(returnString);
+        }
+
+    }
+}
